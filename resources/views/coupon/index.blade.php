87bccd96ed3699a27ed1f9e3350fd6e74c6ce1ad
dynnah <dynnah_hanna@hotmail.com>
2019-11-19 12:15:46 -0300

    altered icons

--- resources/views/coupon/index.blade.php
+++ resources/views/coupon/index.blade.php
@@ -20,11 +20,11 @@
   <div class="col-sm-4">
     <div class="card" style="width: 18rem; margin-bottom:25px;">
       <div class="card-body">
-      <img class="card-img-top" src="/images/img.png" alt="Card image" style="width:100%">
+      <img class="card-img-top" src="/images/cupom.png" alt="Card image" style="width:100%">
       <p class="card-title">{{$coupon->nome}}</p>
       <p class="card-subtitle mb-2 text-muted">{{$coupon->tipo}}</p>
       <p class="card-text">Preço: R$ {{$coupon->preco}}</p>
-      <a href="{{ route('coupons.show', $coupon->id) }}" class="btn btn-primary stretched-link">View Coupon</a>
+      <a href="{{ route('coupons.show', $coupon->id) }}" class="btn btn-primary stretched-link">Ver Cupom</a>
       </div>
     </div>
   </div>
